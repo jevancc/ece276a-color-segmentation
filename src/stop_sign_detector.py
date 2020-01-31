bf1edb3fd5fce3a55e9f2a8758ac62a2d5a9d23f
Jevan Chan <jevan.cnchan@gmail.com>
2020-01-30 22:53:19 -0800

    feat: submission ready

--- src/stop_sign_detector.py
+++ src/stop_sign_detector.py
@@ -5,7 +5,10 @@ Stop Sign Detector
 
 import os, cv2
 from skimage.measure import label, regionprops
-
+import classifier
+import detector
+import image
+import numpy as np
 
 class StopSignDetector():
 
@@ -14,12 +17,13 @@ class StopSignDetector():
 		Initilize your stop sign detector with the attributes you need,
 		e.g., parameters of your classifier
 		'''
-        raise NotImplementedError
+        self.clf = classifier.GaussianNaiveBayes()
+        self.clf.load("./gnb_300000_histeq.pic")
 
     def segment_image(self, img):
         '''
 		Obtain a segmented image using a color classifier,
-		e.g., Logistic Regression, Single Gaussian Generative Model, Gaussian Mixture, 
+		e.g., Logistic Regression, Single Gaussian Generative Model, Gaussian Mixture,
 		call other functions in this class if needed
 
 		Inputs:
@@ -27,9 +31,12 @@ class StopSignDetector():
 		Outputs:
 			mask_img - a binary image with 1 if the pixel in the original image is red and 0 otherwise
 		'''
-        # YOUR CODE HERE
-        raise NotImplementedError
-        return mask_img
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+        X = img.reshape(-1, 3)
+        img_mask = self.clf.predict(X).reshape(img.shape[:2])
+        img_mask = (img_mask == 0).astype(int)
+        return img_mask
 
     def get_bounding_box(self, img):
         '''
@@ -39,27 +46,80 @@ class StopSignDetector():
 		Inputs:
 			img - original image
 		Outputs:
-			boxes - a list of lists of bounding boxes. Each nested list is a bounding box in the form of [x1, y1, x2, y2] 
+			boxes - a list of lists of bounding boxes. Each nested list is a bounding box in the form of [x1, y1, x2, y2]
 			where (x1, y1) and (x2, y2) are the top left and bottom right coordinate respectively. The order of bounding boxes in the list
 			is from left to right in the image.
-			
+
 		Our solution uses xy-coordinate instead of rc-coordinate. More information: http://scikit-image.org/docs/dev/user_guide/numpy_images.html#coordinate-conventions
 		'''
-        # YOUR CODE HERE
-        raise NotImplementedError
+        img_area = img.shape[0] * img.shape[1]
+        ssclf = classifier.SSBBoxDeterministic(img_area)
+
+        img_mask_label = np.zeros((img.shape[0], img.shape[1]))
+
+        img_mask_normal = self.segment_image(img)
+        img_mask_label_normal = label(img_mask_normal, connectivity=1)
+        for region in detector.Region.find(img_mask_label_normal):
+            if ssclf.predict(region):
+                minr, minc, maxr, maxc = region.bbox
+                img_mask_label[minr:maxr, minc:maxc] += region.convex_image
+
+        img = cv2.cvtColor(img, cv2.COLOR_RGB2YCrCb)
+        f = image.build_histogram_equalizer(img[:, :, 0], 255)
+        img[:, :, 0] = f(img[:, :, 0])
+        img = cv2.cvtColor(img, cv2.COLOR_YCrCb2RGB)
+        img_mask_boost = self.segment_image(img)
+        img_mask_label_boost = label(img_mask_boost, connectivity=1)
+        for region in detector.Region.find(img_mask_label_boost):
+            if ssclf.predict(region):
+                minr, minc, maxr, maxc = region.bbox
+                img_mask_label[minr:maxr, minc:maxc] += region.convex_image
+
+        for _ in range(3):
+            img = cv2.cvtColor(img, cv2.COLOR_RGB2HSV)
+            img[:, :, 1] = np.clip((img[:, :, 1].astype(float) * 1.2),
+                                   a_min=0,
+                                   a_max=255).astype(np.uint8)
+            img[:, :, 2] = np.clip((img[:, :, 2].astype(float) * 1.2),
+                                a_min=0,
+                                a_max=255).astype(np.uint8)
+            img = cv2.cvtColor(img, cv2.COLOR_HSV2RGB)
+            img_mask_boost = self.segment_image(img)
+            img_mask_label_boost = label(img_mask_boost, connectivity=1)
+            for region in detector.Region.find(img_mask_label_boost):
+                if ssclf.predict(region):
+                    minr, minc, maxr, maxc = region.bbox
+                    img_mask_label[minr:maxr, minc:maxc] += region.convex_image
+
+        boxes = []
+        img_mask_label = (img_mask_label != 0).astype(int)
+        img_mask_label = label(img_mask_label, connectivity=1)
+        for region in detector.Region.find(img_mask_label):
+            if ssclf.predict(region):
+                minr, minc, maxr, maxc = region.bbox
+                minx, maxx = minc, maxc
+                miny, maxy = img.shape[0] - maxr, img.shape[0] - minr
+
+                boxes.append([minx, miny, maxx, maxy])
+
         return boxes
 
 
 if __name__ == '__main__':
-    folder = "trainset"
     my_detector = StopSignDetector()
-    for filename in os.listdir(folder):
+
+    # folder = "trainset"
+    # files = [os.path.join(folder, filename) for filename in os.listdir(folder)]
+    files = ['../trainset/1.jpg']
+    for filename in files:
         # read one test image
-        img = cv2.imread(os.path.join(folder, filename))
+        img = cv2.imread(filename)
         cv2.imshow('image', img)
         cv2.waitKey(0)
         cv2.destroyAllWindows()
 
+        mask_img = my_detector.segment_image(img)
+        boxes = my_detector.get_bounding_box(img)
         #Display results:
         #(1) Segmented images
         #	 mask_img = my_detector.segment_image(img)
